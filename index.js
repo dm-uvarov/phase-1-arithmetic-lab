c3329a46512b1f7247de0213fe1e37aac4ecfec1
Jimmy <dm.a.uvarov@gmail.com>
2022-01-10 22:12:02 -0500

    commit tree a310dfb885fab94a6771156f1f90b19299a20f7a

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 
 // Write your code here
+/*
+multiply
+
+num1 and num2
+
+random
+
+mod 
+
+set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+
+
+Create a variable called max that finds the highest number in a set; the value returned should be 20.
+*/
+// generates positive integer number that canu be represented by standart integer representation as 2^53 - 1 
+
+let num1 = Math.floor(Math.random()*50 +1);
+let num2 = 62/num1;
+let multiply = Math.round(num1*num2);
+
+
+let num4 =  Math.floor(Math.random()*50 +1);
+let num3 = Math.round(4*num4/(num4 - 1));
+let mod = num3%num4;
+
+let random = Math.floor(Math.pow(2, Math.ceil(Math.random()*54)));
+
+
+let max = Math.max(7,9,15,20);
